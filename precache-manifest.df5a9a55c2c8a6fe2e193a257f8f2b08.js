619eeb3d92cd31fa2892f2225dabe555f0a52585
Angelovski <marko.angelovski@gmail.com>
2020-08-10 02:06:57 +0200

    Updates

---
+++ precache-manifest.df5a9a55c2c8a6fe2e193a257f8f2b08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0470a0333fceea72e487e10efddfa8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7d9a11636e213f114d4",
+    "url": "/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "6e630c6170e6788a4af8",
+    "url": "/static/css/main.28509fd7.chunk.css"
+  },
+  {
+    "revision": "b7d9a11636e213f114d4",
+    "url": "/static/js/2.0a333a60.chunk.js"
+  },
+  {
+    "revision": "89f83aeb343fac1e04aee759a2e3d64f",
+    "url": "/static/js/2.0a333a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e630c6170e6788a4af8",
+    "url": "/static/js/main.1eff0662.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
